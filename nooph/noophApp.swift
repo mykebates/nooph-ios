88cbd4641b1f118ef739e7ec9cec419c9dde1d67
Myke Bates <contact@mykebates.com>
2025-05-19 18:49:08 -0500

    Initial Commit

---
+++ nooph/noophApp.swift
@@ -0,0 +1,17 @@
+//
+//  noophApp.swift
+//  nooph
+//
+//  Created by Myke Bates on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct noophApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
